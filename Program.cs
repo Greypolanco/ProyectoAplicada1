9bd57d9c13a75ddfbee231af13fd4d9c4a33c271
Greilyn Polanco <greilyntejada03@gmail.com>
2023-01-28 13:04:09 -0400

    Realizando conexion SQlite e Inyectando BLL

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using ProyectoAplicada.Data;
-
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+
+builder.Services.AddDbContext<Contexto>(options =>
+options.UseSqlite(ConStr)
+);
+
+builder.Services.AddScoped<OcupacionesBLL>();
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
