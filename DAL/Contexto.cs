ed37b088e0f982a0568f09321e4d755bb022c619
Greilyn Polanco <greilyntejada03@gmail.com>
2023-02-04 15:36:43 -0400

    Agregando Prestamo al Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -4,6 +4,7 @@ public class Contexto : DbContext
 {
     public DbSet<Ocupaciones> Ocupaciones {get; set;}
     public DbSet<Person> Person {get; set;}
+    public DbSet<Lending> Lending {get; set;}
 
     public Contexto (DbContextOptions<Contexto> options): base(options)
     {
