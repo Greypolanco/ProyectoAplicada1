b5c21f11d1260761fbeb07270cbf58fa8522265c
Greilyn Polanco <greilyntejada03@gmail.com>
2023-02-27 21:54:32 -0400

    inicializando el detalle

--- Models/Payments.cs
+++ Models/Payments.cs
@@ -1,12 +1,14 @@
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 public class Payments
 {
     [Key]
-
     public int PaymentId { get; set; }
     public DateTime Date { get; set; }
     public int PersonId { get; set; }
     public string? Conceit { get; set; }
     public int Total { get; set; }
+
+    [ForeignKey("PaymentId")]
+    public virtual List<PaymentsDetalle> PaymentsDetalle {get; set;}
 }
