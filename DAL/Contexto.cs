d5cbabe87bfdb31fac94c8fe6e2c74304bfd0015
Greilyn Polanco <greilyntejada03@gmail.com>
2023-02-27 21:10:16 -0400

    Agregando mi modelo al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -5,7 +5,7 @@ public class Contexto : DbContext
     public DbSet<Ocupaciones> Ocupaciones {get; set;}
     public DbSet<Person> Person {get; set;}
     public DbSet<Lending> Lending {get; set;}
-
+    public DbSet<Payments> Payment { get; set; }
     public Contexto (DbContextOptions<Contexto> options): base(options)
     {
     }
