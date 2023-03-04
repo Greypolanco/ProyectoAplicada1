1b51b44d098e09a580bc1cc074567dc56d8ea0a8
Greilyn Polanco <greilyntejada03@gmail.com>
2023-03-03 21:45:05 -0400

    quitando constructores del models

--- Models/PaymentsDetalle.cs
+++ Models/PaymentsDetalle.cs
@@ -9,18 +9,4 @@ public class PaymentsDetalle
     public int LoanId { get; set; }
     public int PaidValue { get; set; }
 
-    public PaymentsDetalle()
-    {
-        PaymentDetalleId =0;
-        PaymentId = 0;
-        LoanId = 0;
-        PaidValue =0;
-    }
-    public PaymentsDetalle(int paymentId, int loanId, int paidValue)
-    {
-        PaymentDetalleId =0;
-        PaymentId = paymentId;
-        LoanId = loanId;
-        PaidValue = paidValue;
-    }
 }
